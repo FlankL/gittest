1393193985152b1f2470cc5ff9c21f5df97a5398
flank_glb <flank_glb@atguigu.com>
2019-04-19 13:11:41 +0800

    提交人：李四 提交原因：添加了李四跑的方法 提交时间：2019\4\19 13:11

--- src/main/java/com/Person.java
+++ src/main/java/com/Person.java
@@ -3,7 +3,12 @@ package com;
 import javax.xml.bind.SchemaOutputResolver;
 
 public class Person {
+
     public void run (){
         System.out.println("zxhangsan run ");
+    }
+
+    public void run2(){
+        System.out.println("lisi run");
     }
 }
