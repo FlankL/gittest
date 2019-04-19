a4454c9a2744c17b18f45c5bf4637da134245073
flank_glb <flank_glb@atguigu.com>
2019-04-19 14:08:21 +0800

    提交人:flank 提交原因:添加了张三跑的注释 提交时间:2019\4\19 14:08

--- src/main/java/com/Person.java
+++ src/main/java/com/Person.java
@@ -3,6 +3,9 @@ package com;
 import javax.xml.bind.SchemaOutputResolver;
 
 public class Person {
+    /**
+     * 张三跑
+     */
     public void run (){
         System.out.println("zxhangsan run ");
     }
